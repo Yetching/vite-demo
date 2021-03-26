f56e70e502b2ae08b60132cce1e565793d5772b7
Yetching <51228182+Yetching@users.noreply.github.com>
2021-03-26 14:09:17 +0800

    first commit

---
+++ do-css.js
@@ -0,0 +1,3 @@
+import classes from '/eg.module.css';
+
+document.getElementById('eg').className = classes.red;
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="eg">测试CSS-module</div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import imgUrl from './assets/logo.png'
+
+onMounted(() => {
+  console.log(imgUrl)
+})
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
